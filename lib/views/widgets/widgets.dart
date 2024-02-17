5fbbb371c07ba343172b839ec1c1f11fcf94cfd1
abel3ri <abelmerete@yahoo.com>
2024-02-17 17:24:14 +0300

    Add: Widgets file for reusable widgets

---
+++ lib/views/widgets/widgets.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+final List<Widget> carouselItems = [
+  Container(
+    decoration: BoxDecoration(
+      color: Colors.blueAccent,
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+    ),
+    child: Center(
+      child: Text("Item 1"),
+    ),
+  ),
+  Container(
+    decoration: BoxDecoration(
+      color: Colors.blueAccent,
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+    ),
+    child: Center(
+      child: Text("Item 2"),
+    ),
+  ),
+  Container(
+    decoration: BoxDecoration(
+      color: Colors.blueAccent,
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+    ),
+    child: Center(
+      child: Text("Item 3"),
+    ),
+  )
+];
