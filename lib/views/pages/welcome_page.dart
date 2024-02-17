24a75068f83ad06704ddb5f0a5c89473e2c19421
abel3ri <abelmerete@yahoo.com>
2024-02-17 17:43:59 +0300

    Add: active carousel item button

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,6 +1,9 @@
+import 'package:dbu_gym/controllers/providers/carousel_provider.dart';
 import 'package:dbu_gym/views/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:provider/provider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -13,34 +16,53 @@ class WelcomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              "Welcome!",
-              style: textTheme.headlineLarge!.copyWith(
-                fontWeight: FontWeight.bold,
-              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Welcome!",
+                  style: textTheme.headlineLarge!.copyWith(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 48,
+                  ),
+                ),
+                Text(
+                  "DBU Gym.",
+                  style: textTheme.headlineMedium!.copyWith(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              "DBU Gym.",
-              style: textTheme.headlineMedium!.copyWith(
-                color: Theme.of(context).primaryColor,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            CarouselSlider(
-              carouselController: carouselController,
-              items: carouselItems,
-              options: CarouselOptions(
-                aspectRatio: 16 / 9,
-                autoPlay: true,
-                height: 200,
-                viewportFraction: 0.9,
-                enlargeCenterPage: true,
-                enableInfiniteScroll: true,
-              ),
-            ),
+            Column(
+              children: [
+                CarouselSlider(
+                  carouselController: carouselController,
+                  items: carouselItems,
+                  options: CarouselOptions(
+                    aspectRatio: 16 / 9,
+                    autoPlay: true,
+                    height: 200,
+                    viewportFraction: 0.9,
+                    enlargeCenterPage: true,
+                    enableInfiniteScroll: true,
+                    onPageChanged: (index, reason) =>
+                        Provider.of<CarouselProvider>(context, listen: false)
+                            .changeItemIndex(index),
+                  ),
+                ),
+                SizedBox(height: 10),
+                AnimatedSmoothIndicator(
+                  activeIndex: Provider.of<CarouselProvider>(context)
+                      .currentCarouselItemIndex,
+                  count: carouselItems.length,
+                ),
+              ],
+            )
           ],
         ),
       ),
