ac735cf24a48e65a97954afe70d2bd0b6901bcaa
abel3ri <abelmerete@yahoo.com>
2024-02-17 20:54:44 +0300

    Modify: dummy content for carousel

--- lib/controllers/providers/carousel_provider.dart
+++ lib/controllers/providers/carousel_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CarouselProvider with ChangeNotifier {
-  int _currentCarouselItemIndex = 0;
+  int _currentCarouselItemIndex = 1;
 
   get currentCarouselItemIndex => _currentCarouselItemIndex;
 
--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,5 +1,4 @@
 import 'package:dbu_gym/controllers/providers/carousel_provider.dart';
-import 'package:dbu_gym/views/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:provider/provider.dart';
@@ -25,6 +24,7 @@ class WelcomePage extends StatelessWidget {
                 Text(
                   "Welcome!",
                   style: textTheme.headlineLarge!.copyWith(
+                    color: Colors.grey.shade700,
                     fontWeight: FontWeight.bold,
                     fontSize: 48,
                   ),
@@ -40,12 +40,51 @@ class WelcomePage extends StatelessWidget {
             ),
             Column(
               children: [
-                CarouselSlider(
+                CarouselSlider.builder(
                   carouselController: carouselController,
-                  items: carouselItems,
+                  itemCount: 3,
+                  itemBuilder: (context, index, realIndex) => Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context)
+                          .textTheme
+                          .bodyLarge!
+                          .color!
+                          .withAlpha(230),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(
+                            left: 10,
+                            top: 10,
+                          ),
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          child: Text(
+                            "Achieve Your Fitness Goals With Us.",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyLarge!
+                                .copyWith(color: Colors.black),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 3,
+                            softWrap: true,
+                          ),
+                        ),
+                        Image.asset(
+                          alignment: Alignment.centerRight,
+                          "assets/images/fitness.png",
+                        ),
+                      ],
+                    ),
+                  ),
                   options: CarouselOptions(
                     aspectRatio: 16 / 9,
-                    autoPlay: true,
                     height: 200,
                     viewportFraction: 0.9,
                     enlargeCenterPage: true,
@@ -59,10 +98,35 @@ class WelcomePage extends StatelessWidget {
                 AnimatedSmoothIndicator(
                   activeIndex: Provider.of<CarouselProvider>(context)
                       .currentCarouselItemIndex,
-                  count: carouselItems.length,
+                  count: 3,
+                  effect: ExpandingDotsEffect(
+                    activeDotColor: Theme.of(context).primaryColor,
+                    dotHeight: 10,
+                  ),
+                ),
+                // SizedBox()
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FilledButton(
+                  onPressed: () {},
+                  child: Text("Sign up"),
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all(Size(120, 44)),
+                  ),
+                ),
+                SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Login"),
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all(Size(120, 44)),
+                  ),
                 ),
               ],
-            )
+            ),
           ],
         ),
       ),
