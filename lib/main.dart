5843fad27dfef95e51d001d7279e1942b7ddab35
abel3ri <abelmerete@yahoo.com>
2024-02-17 17:44:15 +0300

    Modify: minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:dbu_gym/controllers/providers/carousel_provider.dart';
 import 'package:dbu_gym/firebase_options.dart';
 import 'package:dbu_gym/utils/theme.dart';
 import 'package:dbu_gym/views/pages/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,12 +12,17 @@ void main(List<String> args) async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      home: WelcomePage(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CarouselProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.system,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        home: WelcomePage(),
+      ),
     ),
   );
 }
