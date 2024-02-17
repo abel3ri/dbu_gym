ffdbb97aca536527c00a5892516babc9bd060633
abel3ri <abelmerete@yahoo.com>
2024-02-17 17:25:37 +0300

    Modify: modularize app

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,21 @@
 import 'package:dbu_gym/firebase_options.dart';
 import 'package:dbu_gym/utils/theme.dart';
+import 'package:dbu_gym/views/pages/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      home: Scaffold(
-        body: Center(
-          child: Column(children: [
-            Switch(value: false, onChanged: (valye) {}),
-            FilledButton(
-              onPressed: () {},
-              child: Text("Elevated Button"),
-            ),
-          ]),
-        ),
-      ),
-    );
-  }
+      home: WelcomePage(),
+    ),
+  );
 }
