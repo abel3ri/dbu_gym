488fa88d362958d378f7c35cd67031b7f9f5f29c
abel3ri <abelmerete@yahoo.com>
2024-02-17 22:20:42 +0300

    Add: page not found screen

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,32 +1,34 @@
-import "dart:js";
-
 import "package:dbu_gym/views/pages/login_page.dart";
+import "package:dbu_gym/views/pages/not_found_page.dart";
 import "package:dbu_gym/views/pages/sign_up_intermediate_page.dart";
 import "package:dbu_gym/views/pages/sign_up_page.dart";
 import "package:dbu_gym/views/pages/welcome_page.dart";
 import "package:go_router/go_router.dart";
 
 class AppRouter {
-  static final router = GoRouter(routes: [
-    GoRoute(
-      path: "/",
-      name: "welcome",
-      builder: (context, state) => WelcomePage(),
-    ),
-    GoRoute(
-      path: "/login",
-      name: "login",
-      builder: (context, state) => LoginPage(),
-    ),
-    GoRoute(
-      path: "/signup",
-      name: "signup",
-      builder: (context, state) => SignUpPage(),
-    ),
-    GoRoute(
-      path: "/signup-intermediate",
-      name: "signup-intermediate",
-      builder: (context, state) => SignUpIntermediatePage(),
-    ),
-  ]);
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: "/",
+        name: "welcome",
+        builder: (context, state) => WelcomePage(),
+      ),
+      GoRoute(
+        path: "/login",
+        name: "login",
+        builder: (context, state) => LoginPage(),
+      ),
+      GoRoute(
+        path: "/signup",
+        name: "signup",
+        builder: (context, state) => SignUpPage(),
+      ),
+      GoRoute(
+        path: "/signup-intermediate",
+        name: "signup-intermediate",
+        builder: (context, state) => SignUpIntermediatePage(),
+      ),
+    ],
+    errorBuilder: (context, state) => PageNotFound(),
+  );
 }
