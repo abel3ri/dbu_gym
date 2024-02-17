d4d633d805ac60d15495d17a586fc8cf13f239e6
abel3ri <abelmerete@yahoo.com>
2024-02-17 21:52:10 +0300

    Add: carousel content

---
+++ lib/utils/constants.dart
@@ -0,0 +1,14 @@
+final List<Map<String, dynamic>> carouselContent = [
+  {
+    "headline": "Manage Your Fitness Plan.",
+    "image_path": "assets/images/fitness3.png",
+  },
+  {
+    "headline": "Achieve Your Fitness Goals With Us.",
+    "image_path": "assets/images/fitness1.png",
+  },
+  {
+    "headline": "Explore Our Variety Of Gym Equipments.",
+    "image_path": "assets/images/fitness2.png",
+  },
+];
