8d57be9585a84d29c2edc23cabd2a388d0e57cd6
abel3ri <abelmerete@yahoo.com>
2024-02-20 19:51:15 +0300

    Add: user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,1 +1,19 @@
+class User {
+  late String firstName;
+  late String lastName;
+  late String email;
+  late String password;
+  late String gymStartDate;
+  late String gymEndDate;
+  late String workoutShift;
 
+  User({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+    required this.gymStartDate,
+    required this.gymEndDate,
+    required this.workoutShift,
+  });
+}
