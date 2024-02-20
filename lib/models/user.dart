bb9d67a1fe4213b40f703c0b03833ccaecf3ff8e
abel3ri <abelmerete@yahoo.com>
2024-02-20 21:48:03 +0300

    Add: implement sign up and login

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,4 +1,8 @@
-class User {
+import 'package:dbu_gym/utils/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fpdart/fpdart.dart';
+
+class GymUser {
   late String firstName;
   late String lastName;
   late String email;
@@ -7,7 +11,7 @@ class User {
   late String gymEndDate;
   late String workoutShift;
 
-  User({
+  GymUser({
     required this.firstName,
     required this.lastName,
     required this.email,
@@ -16,4 +20,36 @@ class User {
     required this.gymEndDate,
     required this.workoutShift,
   });
+
+  Future<Either<String, User>> signUpUserWithEmailAndPassword() async {
+    try {
+      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      User? user = userCredential.user;
+      if (userCredential.user != null) return right(user!);
+      throw "User not created.";
+    } on FirebaseAuthException catch (err) {
+      return left(err.message!);
+    } catch (err) {
+      return left(err.toString());
+    }
+  }
+
+  Future<Either<String, User>> signInUserWithEmailAndPassword() async {
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      User? user = userCredential.user;
+      if (userCredential.user != null) return right(user!);
+      throw "Unable to sign in user.";
+    } on FirebaseAuthException catch (err) {
+      return left(err.message!);
+    } catch (err) {
+      return left(err.toString());
+    }
+  }
 }
