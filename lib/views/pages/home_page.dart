914805a72e5d1baf64f1ce308a0a3ca656c6719e
abel3ri <abelmerete@yahoo.com>
2024-02-21 19:20:35 +0300

    Minor changes

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:dbu_gym/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -15,6 +16,7 @@ class HomePage extends StatelessWidget {
             ElevatedButton(
               onPressed: () async {
                 await auth.signOut();
+                GoRouter.of(context).pushReplacementNamed("splash");
               },
               child: Text("Sign out"),
             ),
--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: must_be_immutable
-
 import 'package:dbu_gym/providers/carousel_provider.dart';
 import 'package:dbu_gym/utils/constants.dart';
 import 'package:dbu_gym/views/widgets/signup_login_button.dart';
