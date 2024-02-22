7230da9219e4157a869380c46973f367ee4673ee
abel3ri <abelmerete@yahoo.com>
2024-02-22 21:37:16 +0300

    Finish: pricing page for 1-3Days and 4-6 Days

--- lib/views/pages/pricing_page.dart
+++ lib/views/pages/pricing_page.dart
@@ -2,6 +2,7 @@ import 'package:dbu_gym/providers/pricing_provider.dart';
 import 'package:dbu_gym/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -64,137 +65,10 @@ class PricingPage extends StatelessWidget {
                     ],
                   ),
                   SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                  CarouselSlider.builder(
-                    itemCount: pricingContent["1-3Days"]!.length,
-                    itemBuilder: (context, index, realIndex) => LayoutBuilder(
-                      builder: (context, constraints) => Card(
-                        elevation: 2,
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                            left: 12,
-                            right: 12,
-                            top: 12,
-                            // bottom: 8,
-                          ),
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text("Workout type",
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodySmall!
-                                          .copyWith(
-                                            fontWeight: FontWeight.bold,
-                                          )),
-                                  SizedBox(
-                                      width: MediaQuery.of(context).size.width *
-                                          0.02),
-                                  Text(
-                                    pricingContent['1-3Days']![index]
-                                        ['workoutType']!,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyMedium!
-                                        .copyWith(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .primary,
-                                          fontWeight: FontWeight.w900,
-                                        ),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      0.04),
-                              Image.asset(
-                                pricingContent['1-3Days']![index]['imagePath']!,
-                                height: constraints.maxHeight * 0.6,
-                              ),
-                              SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      0.04),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "Price for Insiders",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodySmall!
-                                        .copyWith(
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                  ),
-                                  Text(
-                                    pricingContent['1-3Days']![index]
-                                        ['insidersPrice']!,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyMedium!
-                                        .copyWith(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .primary,
-                                          fontWeight: FontWeight.w900,
-                                        ),
-                                  )
-                                ],
-                              ),
-                              SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      0.02),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "Price for Outsiders",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodySmall!
-                                        .copyWith(
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                  ),
-                                  Text(
-                                    pricingContent['1-3Days']![index]
-                                        ['outsidersPrice']!,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyMedium!
-                                        .copyWith(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .primary,
-                                          fontWeight: FontWeight.w900,
-                                        ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    options: CarouselOptions(
-                      onPageChanged: (index, reason) {
-                        Provider.of<PricingProvider>(context, listen: false)
-                            .changeActiveIndex(index);
-                      },
-                      aspectRatio: 16 / 9,
-                      height: MediaQuery.of(context).size.height * 0.6,
-                      initialPage: 0,
-                      viewportFraction: 1,
-                      enlargeCenterPage: true,
-                      enableInfiniteScroll: true,
-                      enlargeFactor: 0,
-                    ),
-                  ),
+                  if (pricingProvider.oneThreeDays)
+                    OneThreeCarouselSlider()
+                  else
+                    FourSixCarouselSlider(),
                   SizedBox(height: MediaQuery.of(context).size.height * 0.02),
                   Center(
                     child: AnimatedSmoothIndicator(
@@ -211,6 +85,277 @@ class PricingPage extends StatelessWidget {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class FourSixCarouselSlider extends StatelessWidget {
+  const FourSixCarouselSlider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider.builder(
+      itemCount: pricingContent["4-6Days"]!.length,
+      itemBuilder: (context, index, realIndex) => LayoutBuilder(
+        builder: (context, constraints) => Stack(
+          alignment: Alignment(0, 0.7),
+          children: [
+            Card(
+              elevation: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Workout type",
+                            style:
+                                Theme.of(context).textTheme.bodySmall!.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                        // SizedBox(width: MediaQuery.of(context).size.width * 0.02),
+                        SizedBox(width: constraints.maxHeight * 0.02),
+                        Text(
+                          pricingContent['4-6Days']![index]['workoutType']!,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontWeight: FontWeight.w900,
+                              ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+                    if (pricingContent['4-6Days']![index]['imagePath'].length >
+                        1)
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          // since pricing image 3 a bit larger in width, im decreasing the width by 5%
+                          Image.asset(
+                            pricingContent['4-6Days']![index]['imagePath'][0]!,
+                            height: pricingContent['4-6Days']![index]
+                                        ['imagePath'][0]!
+                                    .toString()
+                                    .contains("_3")
+                                ? constraints.maxHeight * 0.35
+                                : constraints.maxHeight * 0.4,
+                          ),
+                          Image.asset(
+                            pricingContent['4-6Days']![index]['imagePath'][1]!,
+                            height: constraints.maxHeight * 0.4,
+                          ),
+                        ],
+                      )
+                    else
+                      Image.asset(
+                        pricingContent['4-6Days']![index]['imagePath'][0],
+                        height: constraints.maxHeight * 0.6,
+                      ),
+                    Expanded(child: SizedBox()),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Price for Insiders",
+                          style:
+                              Theme.of(context).textTheme.bodySmall!.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                        ),
+                        Text(
+                          pricingContent['4-6Days']![index]['insidersPrice']!,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontWeight: FontWeight.w900,
+                              ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Price for Outsiders",
+                          style:
+                              Theme.of(context).textTheme.bodySmall!.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                        ),
+                        Text(
+                          pricingContent['4-6Days']![index]['outsidersPrice']!,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontWeight: FontWeight.w900,
+                              ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Text(
+              pricingContent["4-6Days"]![index]["noWorkingDays"],
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    fontFamily: GoogleFonts.swankyAndMooMoo().fontFamily,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.error,
+                  ),
+            )
+          ],
+        ),
+      ),
+      options: CarouselOptions(
+        onPageChanged: (index, reason) {
+          Provider.of<PricingProvider>(context, listen: false)
+              .changeActiveIndex(index);
+        },
+        aspectRatio: 16 / 9,
+        height: MediaQuery.of(context).size.height * 0.6,
+        initialPage: 0,
+        viewportFraction: 1,
+        enlargeCenterPage: true,
+        enableInfiniteScroll: true,
+        enlargeFactor: 0,
+      ),
+    );
+  }
+}
+
+class OneThreeCarouselSlider extends StatelessWidget {
+  const OneThreeCarouselSlider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider.builder(
+      itemCount: pricingContent["1-3Days"]!.length,
+      itemBuilder: (context, index, realIndex) => LayoutBuilder(
+        builder: (context, constraints) => Stack(
+          alignment: Alignment(0, 0.7),
+          children: [
+            Card(
+              elevation: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Workout type",
+                            style:
+                                Theme.of(context).textTheme.bodySmall!.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.02),
+                        Text(
+                          pricingContent['1-3Days']![index]['workoutType']!,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontWeight: FontWeight.w900,
+                              ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+                    Image.asset(
+                      pricingContent['1-3Days']![index]['imagePath']!,
+                      height: constraints.maxHeight * 0.6,
+                    ),
+                    Expanded(child: SizedBox()),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Price for Insiders",
+                          style:
+                              Theme.of(context).textTheme.bodySmall!.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                        ),
+                        Text(
+                          pricingContent['1-3Days']![index]['insidersPrice']!,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontWeight: FontWeight.w900,
+                              ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Price for Outsiders",
+                          style:
+                              Theme.of(context).textTheme.bodySmall!.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                        ),
+                        Text(
+                          pricingContent['1-3Days']![index]['outsidersPrice']!,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontWeight: FontWeight.w900,
+                              ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Text(
+              pricingContent["1-3Days"]![index]["noWorkingDays"],
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    fontFamily: GoogleFonts.swankyAndMooMoo().fontFamily,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.error,
+                  ),
+            ),
+          ],
+        ),
+      ),
+      options: CarouselOptions(
+        onPageChanged: (index, reason) {
+          Provider.of<PricingProvider>(context, listen: false)
+              .changeActiveIndex(index);
+        },
+        aspectRatio: 16 / 9,
+        height: MediaQuery.of(context).size.height * 0.6,
+        initialPage: 0,
+        viewportFraction: 1,
+        enlargeCenterPage: true,
+        enableInfiniteScroll: true,
+        enlargeFactor: 0,
       ),
     );
   }
