b66c729e85bdc08a4c3e88f3d348d192c14df4f8
abel3ri <abelmerete@yahoo.com>
2024-02-22 19:38:40 +0300

    Add: add switch toggle functions

--- lib/providers/pricing_provider.dart
+++ lib/providers/pricing_provider.dart
@@ -1,3 +1,30 @@
 import 'package:flutter/material.dart';
 
-class PricingProvider with ChangeNotifier {}
+class PricingProvider with ChangeNotifier {
+  int _activeIndex = 0;
+  bool _oneThreeDays = true;
+  bool _fourSixDays = false;
+
+  void changeActiveIndex(int index) {
+    _activeIndex = index;
+    notifyListeners();
+  }
+
+  void toggleOneThreeDays(bool value) {
+    _oneThreeDays = value;
+    _fourSixDays = !value;
+    notifyListeners();
+  }
+
+  void toggleFourSixDays(bool value) {
+    _fourSixDays = value;
+    _oneThreeDays = !value;
+    notifyListeners();
+  }
+
+  // v
+
+  bool get oneThreeDays => _oneThreeDays;
+  bool get fourSixDays => _fourSixDays;
+  int get activeIndex => _activeIndex;
+}
