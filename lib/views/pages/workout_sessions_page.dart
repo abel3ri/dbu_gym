0f0a45657be4d2693a994008d043cc23b1d51010
abel3ri <abelmerete@yahoo.com>
2024-02-23 19:55:57 +0300

    Modify: separte workout program widget

--- lib/views/pages/workout_sessions_page.dart
+++ lib/views/pages/workout_sessions_page.dart
@@ -1,4 +1,5 @@
 import 'package:dbu_gym/utils/constants.dart';
+import 'package:dbu_gym/views/widgets/workout_program.dart';
 import 'package:flutter/material.dart';
 
 class WorkoutSessionPage extends StatelessWidget {
@@ -54,51 +55,6 @@ class WorkoutSessionPage extends StatelessWidget {
         Divider(),
         SizedBox(height: MediaQuery.of(context).size.height * 0.02),
         WorkoutProgram(programName: "program 3", workoutDays: "tueThuSat"),
-      ],
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class WorkoutProgram extends StatelessWidget {
-  String programName;
-  String workoutDays;
-  WorkoutProgram({
-    super.key,
-    required this.programName,
-    required this.workoutDays,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    print(workoutDays);
-    print(programName);
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          programName,
-          style: Theme.of(context).textTheme.bodyLarge,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            ...workoutSessionContent[workoutDays]![programName]!.map(
-              (content) {
-                return SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.04,
-                  child: Text(
-                    content,
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        // fontSize: 2,
-                        ),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
       ],
     );
   }
