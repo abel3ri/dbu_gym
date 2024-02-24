d44695cdde5cd20ba75a8d60f12386f19919ebe4
abel3ri <abelmerete@yahoo.com>
2024-02-24 23:21:40 +0300

    Modify: show different exercise categories based on selected filter

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,17 +1,45 @@
+import 'package:dbu_gym/providers/home_page_grid_provider.dart';
 import 'package:dbu_gym/utils/constants.dart';
 import 'package:dbu_gym/views/widgets/app_drawer.dart';
+import 'package:dbu_gym/views/widgets/home_page_grid.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // final userId = auth.currentUser!.uid;
+    final categoryProvider = Provider.of<HomePageGridProvider>(context);
 
     return AppZoomDrawer(
-      appBarActions: [],
+      title: Text("DBU Gym"),
+      appBarActions: [
+        PopupMenuButton(
+          onSelected: (value) {
+            // print(value);
+            Provider.of<HomePageGridProvider>(context, listen: false)
+                .changeCategory(value);
+          },
+          initialValue: categoryProvider.selectedCategory,
+          padding: EdgeInsets.fromLTRB(4, 4, 0, 4),
+          itemBuilder: (context) => [
+            PopupMenuItem(
+              value: "muscle",
+              child: Text("Categorize by muscle group"),
+            ),
+            PopupMenuItem(
+              value: "exercise",
+              child: Text("Categorize by exercise type"),
+            ),
+            PopupMenuItem(
+              value: "difficulty",
+              child: Text("Categorize by difficulty"),
+            ),
+          ],
+        ),
+      ],
       mainScreen: DefaultTabController(
         length: 2,
         child: Scaffold(
@@ -27,47 +55,12 @@ class HomePage extends StatelessWidget {
                 Expanded(
                   child: TabBarView(
                     children: [
-                      GridView.builder(
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                        ),
-                        itemCount: exerciseCategories.length,
-                        padding: EdgeInsets.only(left: 8, right: 8, bottom: 8),
-                        itemBuilder: (context, index) {
-                          return GestureDetector(
-                            onTap: () {},
-                            child: Card(
-                              elevation: 0.8,
-                              shape: RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(4)),
-                              ),
-                              child: Center(
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Image.asset(
-                                      exerciseCategories[index]['imagePath']!,
-                                      fit: BoxFit.cover,
-                                    ),
-                                    SizedBox(
-                                      height:
-                                          MediaQuery.of(context).size.height *
-                                              0.01,
-                                    ),
-                                    Text(
-                                      exerciseCategories[index]
-                                          ['exerciseName']!,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodyMedium,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          );
-                        },
+                      HomePageGrid(
+                        category: categoryProvider.selectedCategory == "muscle"
+                            ? muscleGroupCategories
+                            : categoryProvider.selectedCategory == "exercise"
+                                ? exerciseTypeCategories
+                                : difficultyCategories,
                       ),
                       Center(
                         child: TextButton(
