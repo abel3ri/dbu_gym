7ee269dfd50d55d79ff0aec926ceebbeb6b9d2d3
abel3ri <abelmerete@yahoo.com>
2024-02-24 23:22:36 +0300

    Modify: make the grid reusable for each category

--- lib/views/widgets/home_page_grid.dart
+++ lib/views/widgets/home_page_grid.dart
@@ -1,9 +1,13 @@
 import 'package:dbu_gym/utils/constants.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class HomePageGrid extends StatelessWidget {
-  const HomePageGrid({
+  List<Map<String, String>> category;
+  HomePageGrid({
     super.key,
+    required this.category,
   });
 
   @override
@@ -12,13 +16,18 @@ class HomePageGrid extends StatelessWidget {
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
       ),
-      itemCount: muscleGroupCategories.length,
+      itemCount: category.length,
       padding: EdgeInsets.only(left: 8, right: 8, bottom: 8),
       itemBuilder: (context, index) {
         return GestureDetector(
-          onTap: () {},
+          onTap: () {
+            // print("Clicked");
+          },
           child: Card(
             elevation: 0.8,
+            color: category[index]['difficulty'] != null
+                ? difficultyCategoryColors[index].darken(20)
+                : null,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.all(Radius.circular(4)),
             ),
@@ -26,17 +35,31 @@ class HomePageGrid extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image.asset(
-                    muscleGroupCategories[index]['imagePath']!,
-                    fit: BoxFit.cover,
-                  ),
+                  if (category[index]['difficulty'] == null)
+                    Image.asset(
+                      category[index]['imagePath']!,
+                      fit: BoxFit.cover,
+                    )
+                  else
+                    Text(
+                      category[index]['difficulty']!,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            color: Colors.white,
+                          ),
+                    ),
                   SizedBox(
                     height: MediaQuery.of(context).size.height * 0.01,
                   ),
-                  Text(
-                    muscleGroupCategories[index]['exerciseName']!,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
+                  if (category[index]['muscleName'] != null)
+                    Text(
+                      category[index]['muscleName']!,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  if (category[index]['exerciseTypeName'] != null)
+                    Text(
+                      category[index]['exerciseTypeName']!,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
                 ],
               ),
             ),
