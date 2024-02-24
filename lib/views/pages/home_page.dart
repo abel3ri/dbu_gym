2a7d7f385fe429ae6f09d75fa26138eb22f2f767
abel3ri <abelmerete@yahoo.com>
2024-02-24 21:18:16 +0300

    Add: structure the grid layout

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -7,45 +7,62 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // final userId = auth.currentUser!.uid;
+
     return AppZoomDrawer(
-      appBarActions: [
-        Text("Hello"),
-      ],
+      appBarActions: [],
       mainScreen: DefaultTabController(
-        length: 3,
+        length: 2,
         child: Scaffold(
           body: SafeArea(
             child: Column(
               children: [
                 TabBar(
                   tabs: [
-                    Tab(
-                      child: Center(child: Text("Workouts")),
-                    ),
-                    Tab(
-                      child: Center(child: Text("Calculate BMI")),
-                    ),
-                    Tab(
-                      child: Center(child: Text("Subscription")),
-                    )
+                    Tab(child: Center(child: Text("Workouts"))),
+                    Tab(child: Center(child: Text("Calculate BMI"))),
                   ],
                 ),
                 Expanded(
                   child: TabBarView(
                     children: [
-                      Center(
-                        child: TextButton(
-                            onPressed: () async {
-                              await auth.signOut();
-                            },
-                            child: Text("Sign out")),
-                      ),
-                      Center(
-                        child: TextButton(
-                            onPressed: () async {
-                              await auth.signOut();
-                            },
-                            child: Text("Sign out")),
+                      GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          // mainAxisSpacing: 8,
+                          // crossAxisSpacing: 4,
+                        ),
+                        itemCount: exerciseCategories.length,
+                        padding: EdgeInsets.only(left: 8, right: 8, bottom: 8),
+                        itemBuilder: (context, index) {
+                          return Card(
+                            elevation: 0.8,
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4)),
+                            ),
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset(
+                                    exerciseCategories[index]['imagePath']!,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  SizedBox(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.01,
+                                  ),
+                                  Text(
+                                    exerciseCategories[index]['exerciseName']!,
+                                    style:
+                                        Theme.of(context).textTheme.bodyMedium,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
                       ),
                       Center(
                         child: TextButton(
