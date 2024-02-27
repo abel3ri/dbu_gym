ba5a6a5861536ec9e96c58322dd87dc6a2687760
abel3ri <abelmerete@yahoo.com>
2024-02-27 20:25:36 +0300

    Add: add exercise model class

---
+++ lib/models/exercise.dart
@@ -0,0 +1,23 @@
+class Exercise {
+  late String name;
+  late String muscle;
+  late String? equipment;
+  late String difficulty;
+  late String videoUrl;
+  late String explanation;
+  late String beginnerSets;
+  late String intermediateSets;
+  late String expertSets;
+
+  Exercise({
+    required this.name,
+    required this.muscle,
+    required this.equipment,
+    required this.difficulty,
+    required this.explanation,
+    required this.videoUrl,
+    required this.beginnerSets,
+    required this.intermediateSets,
+    required this.expertSets,
+  });
+}
