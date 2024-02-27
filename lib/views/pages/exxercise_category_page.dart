16d98841115527ff8b821f8b950bdfcef58aae0d
abel3ri <abelmerete@yahoo.com>
2024-02-27 22:24:07 +0300

    Add: exercise category page

---
+++ lib/views/pages/exxercise_category_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExerciseCategory extends StatelessWidget {
+  const ExerciseCategory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
