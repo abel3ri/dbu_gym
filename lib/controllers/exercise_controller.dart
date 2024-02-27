6eae3cc1b975fbb9cf9c8ede58dc6b3abbad353b
abel3ri <abelmerete@yahoo.com>
2024-02-27 20:52:30 +0300

    Add: add get exercise by difficulty func

--- lib/controllers/exercise_controller.dart
+++ lib/controllers/exercise_controller.dart
@@ -2,35 +2,70 @@ import 'package:dbu_gym/models/exercise.dart';
 import 'package:dbu_gym/utils/constants.dart';
 import 'package:fpdart/fpdart.dart';
 
-Future<Either<String, List<Exercise>>> getExerciseByMuscleName(
-    String muscleName) async {
-  try {
-    final res = await dio.get(
-      exerciseAPIUrl,
-      queryParameters: {
-        "Muscles": muscleName.replaceAll(" ", "_"),
-      },
-      options: options,
-    );
+class ExerciseController {
+  static Future<Either<String, List<Exercise>>> getExerciseByMuscleName(
+      String muscleName) async {
+    try {
+      final res = await dio.get(
+        exerciseAPIUrl,
+        queryParameters: {
+          "Muscles": muscleName.replaceAll(" ", "_"),
+        },
+        options: options,
+      );
 
-    final data = res.data as List<dynamic>;
-    List<Exercise> exercsies = [];
+      final data = res.data as List<dynamic>;
+      List<Exercise> exercsies = [];
 
-    data.forEach((exercise) {
-      exercsies.add(Exercise(
-        name: exercise['WorkOut'],
-        muscle: exercise['Muscles'],
-        equipment: exercise['Equipment'] ?? "Not required",
-        difficulty: exercise['Intensity_Level'],
-        explanation: exercise['Long Explanation'],
-        beginnerSets: exercise['Beginner Sets'],
-        intermediateSets: exercise['Intermediate Sets'],
-        expertSets: exercise['Expert Sets'],
-        videoUrl: exercise['Video'],
-      ));
-    });
-    return right(exercsies);
-  } catch (err) {
-    return left(err.toString());
+      data.forEach((exercise) {
+        exercsies.add(Exercise(
+          name: exercise['WorkOut'],
+          muscle: exercise['Muscles'],
+          equipment: exercise['Equipment'] ?? "Not required",
+          difficulty: exercise['Intensity_Level'],
+          explanation: exercise['Long Explanation'],
+          beginnerSets: exercise['Beginner Sets'],
+          intermediateSets: exercise['Intermediate Sets'],
+          expertSets: exercise['Expert Sets'],
+          videoUrl: exercise['Video'],
+        ));
+      });
+      return right(exercsies);
+    } catch (err) {
+      return left(err.toString());
+    }
+  }
+
+  static Future<Either<String, List<Exercise>>> getExerciseByDifficulty(
+      String difficulty) async {
+    try {
+      final res = await dio.get(
+        exerciseAPIUrl,
+        queryParameters: {
+          "Muscles": difficulty.replaceAll(" ", "_"),
+        },
+        options: options,
+      );
+
+      final data = res.data as List<dynamic>;
+      List<Exercise> exercsies = [];
+
+      data.forEach((exercise) {
+        exercsies.add(Exercise(
+          name: exercise['WorkOut'],
+          muscle: exercise['Muscles'],
+          equipment: exercise['Equipment'] ?? "Not required",
+          difficulty: exercise['Intensity_Level'],
+          explanation: exercise['Long Explanation'],
+          beginnerSets: exercise['Beginner Sets'],
+          intermediateSets: exercise['Intermediate Sets'],
+          expertSets: exercise['Expert Sets'],
+          videoUrl: exercise['Video'],
+        ));
+      });
+      return right(exercsies);
+    } catch (err) {
+      return left(err.toString());
+    }
   }
 }
