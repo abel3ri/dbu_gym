ae2b9aacaddd672af952591fad639a77037b7641
abel3ri <abelmerete@yahoo.com>
2024-02-28 18:35:49 +0300

    Add: add the provider logic for exercise provider

--- lib/providers/exercise_provider.dart
+++ lib/providers/exercise_provider.dart
@@ -1,3 +1,20 @@
+import 'package:dbu_gym/models/exercise.dart';
 import 'package:flutter/material.dart';
 
-class ExerciseProvider with ChangeNotifier {}
+class ExerciseProvider with ChangeNotifier {
+  List<Exercise> _exercises = [];
+  String? _categoryName;
+
+  set setExercises(List<Exercise> exercises) {
+    _exercises = exercises;
+    notifyListeners();
+  }
+
+  set setCategoryName(String categoryName) {
+    _categoryName = categoryName;
+    notifyListeners();
+  }
+
+  List<Exercise> get exercises => _exercises;
+  String get categoryName => _categoryName!;
+}
