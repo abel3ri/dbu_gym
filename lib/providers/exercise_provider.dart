e3ad6b7aa8fdb503a2aa05e400ebcce006f6bba9
abel3ri <abelmerete@yahoo.com>
2024-03-01 20:25:27 +0300

    Modify: fix error when filtering with equipment

--- lib/providers/exercise_provider.dart
+++ lib/providers/exercise_provider.dart
@@ -11,6 +11,7 @@ class ExerciseProvider with ChangeNotifier {
 
   String? _searchParamter;
   String? _categoryName;
+  List<Exercise>? _exercises;
 
   set setSearchParamter(String value) {
     _searchParamter = value;
@@ -22,10 +23,10 @@ class ExerciseProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  Either<String, List<Exercise>> getExercises() {
+  void getExercises() {
     try {
       if (_categoryName == "muscle") {
-        return right(this
+        _exercises = this
             .allExercises
             .filter((exercise) {
               return (exercise['primaryMuscles'] as List)
@@ -46,9 +47,10 @@ class ExerciseProvider with ChangeNotifier {
                 images: List<String>.from(e['images']),
               ),
             )
-            .toList());
+            .toList();
+        notifyListeners();
       } else if (_categoryName == "difficulty") {
-        return right(this
+        _exercises = this
             .allExercises
             .filter((exercise) {
               return (exercise['level']).contains(_searchParamter);
@@ -68,9 +70,10 @@ class ExerciseProvider with ChangeNotifier {
                 images: List<String>.from(e['images']),
               ),
             )
-            .toList());
+            .toList();
+        notifyListeners();
       } else if (_categoryName == 'exercise') {
-        return right(this
+        _exercises = this
             .allExercises
             .filter((exercise) {
               return (exercise['category']).contains(_searchParamter);
@@ -90,36 +93,41 @@ class ExerciseProvider with ChangeNotifier {
                 images: List<String>.from(e['images']),
               ),
             )
-            .toList());
+            .toList();
+        notifyListeners();
+      } else {
+        // print(_searchParamter);
+        _exercises = this
+            .allExercises
+            .filter((exercise) {
+              if (exercise['equipment'] != null)
+                return exercise['equipment'].contains(_searchParamter);
+              return false;
+            })
+            .toList()
+            .map(
+              (e) => Exercise(
+                name: e['name'],
+                primaryMuscle: e['primaryMuscles'][0],
+                equipment: e['equipment'],
+                level: e['level'],
+                mechanic: e['mechanic'],
+                force: e['force'],
+                secondaryMuscles: List<String>.from(['secondaryMuscles']),
+                instructions: List<String>.from(e['instructions']),
+                category: e['category'],
+                images: List<String>.from(e['images']),
+              ),
+            )
+            .toList();
+        notifyListeners();
       }
-
-      return right(this
-          .allExercises
-          .filter((exercise) {
-            return exercise['name'].contains(_searchParamter);
-            // exercise['equipment'].contains(_searchParamter);
-          })
-          .toList()
-          .map(
-            (e) => Exercise(
-              name: e['name'],
-              primaryMuscle: e['primaryMuscles'][0],
-              equipment: e['equipment'],
-              level: e['level'],
-              mechanic: e['mechanic'],
-              force: e['force'],
-              secondaryMuscles: List<String>.from(['secondaryMuscles']),
-              instructions: List<String>.from(e['instructions']),
-              category: e['category'],
-              images: List<String>.from(e['images']),
-            ),
-          )
-          .toList());
     } catch (err) {
-      return left(err.toString());
+      print(err.toString());
     }
   }
 
   String get searchParamter => _searchParamter!;
   String get categoryName => _categoryName!;
+  List<Exercise> get exercises => _exercises!;
 }
