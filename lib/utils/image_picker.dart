bb450450935cb479bc4433f9b85e2c640ffeb136
abel3ri <abelmerete@yahoo.com>
2024-03-02 22:01:47 +0300

    Fix: catch error when picking profile image

--- lib/utils/image_picker.dart
+++ lib/utils/image_picker.dart
@@ -4,7 +4,7 @@ import 'package:image_picker/image_picker.dart';
 Future<Either<String, XFile>> pickImageFromCamera() async {
   try {
     XFile? image = await ImagePicker().pickImage(source: ImageSource.camera);
-    if (image == null) throw left("Image not selected");
+    if (image == null) return left("Image not selected!");
     return right(image);
   } catch (err) {
     throw left(err.toString());
@@ -14,7 +14,7 @@ Future<Either<String, XFile>> pickImageFromCamera() async {
 Future<Either<String, XFile>> pickImageFromGallery() async {
   try {
     XFile? image = await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (image == null) throw left("Image not selected!");
+    if (image == null) return left("Image not selected!");
     return right(image);
   } catch (err) {
     throw left(err.toString());
