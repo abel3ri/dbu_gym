d845558aa7d2effcf903f8da64545e1d41981929
abel3ri <abelmerete@yahoo.com>
2024-03-03 15:04:27 +0300

    Add: begin exercise details page

--- lib/views/pages/exercise_details_page.dart
+++ lib/views/pages/exercise_details_page.dart
@@ -1,13 +1,49 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:dbu_gym/providers/exercise_provider.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 class ExerciseDetailsPage extends StatelessWidget {
   const ExerciseDetailsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final exerciseProvider = Provider.of<ExerciseProvider>(context);
     return Scaffold(
-      body: Center(
-        child: Text("Details Page."),
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          style: ButtonStyle(
+            overlayColor: MaterialStatePropertyAll(
+              Colors.transparent,
+            ),
+          ),
+          onPressed: () {
+            GoRouter.of(context).pop();
+          },
+          icon: Icon(Icons.arrow_back_ios_new),
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              child: exerciseProvider.gifBytes.isEmpty
+                  ? Center(child: Text("No GIF"))
+                  : Image.memory(Uint8List.fromList(exerciseProvider.gifBytes),
+                      height: 200),
+            ),
+            Container(
+              child: TextButton(
+                  onPressed: () async {
+                    await exerciseProvider.convertJpgToPdf();
+                  },
+                  child: Text("Click")),
+            ),
+          ],
+        ),
       ),
     );
   }
