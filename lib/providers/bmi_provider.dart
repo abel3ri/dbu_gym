5bafa7992c125fb38d42f5927e9b154f78225b0e
abel3ri <abelmerete@yahoo.com>
2024-03-06 23:49:12 +0300

    Modify: minor changes

--- lib/providers/bmi_provider.dart
+++ lib/providers/bmi_provider.dart
@@ -6,7 +6,7 @@ class BMIProvider with ChangeNotifier {
   TextEditingController _weightController = TextEditingController();
   TextEditingController _heightController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-  double? _bmi;
+  double _bmi = 0;
 
   void calcBMI() {
     _bmi = (double.parse(_weightController.text)) /
@@ -17,9 +17,9 @@ class BMIProvider with ChangeNotifier {
 
   String bmiMessageMap(double bmi) {
     if (bmi > 22) {
-      return "You are over weight, you should take less calorie foods.";
+      return "It seems you are over weight, you should take less calorie foods.";
     } else if (bmi < 18) {
-      return "You are under weight, you should take more calorie foods.";
+      return "It seems you are under weight, you should take more calorie foods.";
     }
     return "You are in good shape. Keep doing what you're doing.";
   }
@@ -27,5 +27,5 @@ class BMIProvider with ChangeNotifier {
   TextEditingController get weightController => _weightController;
   TextEditingController get heightController => _heightController;
   GlobalKey<FormState> get formKey => _formKey;
-  double get bmi => _bmi!;
+  double get bmi => _bmi;
 }
