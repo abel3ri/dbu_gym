ab15f31a86568ea857801ea80deafdd81b2c4480
abel3ri <abelmerete@yahoo.com>
2024-03-06 23:27:04 +0300

    Add: descriptive message after calculating bmi

--- lib/providers/bmi_provider.dart
+++ lib/providers/bmi_provider.dart
@@ -5,12 +5,27 @@ import 'package:flutter/material.dart';
 class BMIProvider with ChangeNotifier {
   TextEditingController _weightController = TextEditingController();
   TextEditingController _heightController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  double? _bmi;
 
-  double calcBMI() {
-    return (double.parse(_weightController.text)) /
+  void calcBMI() {
+    _bmi = (double.parse(_weightController.text)) /
         (pow(double.parse(_heightController.text), 2));
+
+    notifyListeners();
+  }
+
+  String bmiMessageMap(double bmi) {
+    if (bmi > 22) {
+      return "You are over weight, you should take less calorie foods.";
+    } else if (bmi < 18) {
+      return "You are under weight, you should take more calorie foods.";
+    }
+    return "You are in good shape. Keep doing what you're doing.";
   }
 
   TextEditingController get weightController => _weightController;
-  TextEditingController get heightController => _weightController;
+  TextEditingController get heightController => _heightController;
+  GlobalKey<FormState> get formKey => _formKey;
+  double get bmi => _bmi!;
 }
