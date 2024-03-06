e1ad43246e0d34d6142fda8e35a36e82f6af2b80
abel3ri <abelmerete@yahoo.com>
2024-03-06 23:25:39 +0300

    Modify: wrap the inputs inside a form to run validators

--- lib/views/pages/calc_bmi_page.dart
+++ lib/views/pages/calc_bmi_page.dart
@@ -1,11 +1,14 @@
+import 'package:dbu_gym/providers/bmi_provider.dart';
 import 'package:dbu_gym/views/widgets/bmi_input_field.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CalcBMIPage extends StatelessWidget {
   const CalcBMIPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final bmiProvider = Provider.of<BMIProvider>(context);
     return Scaffold(
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
@@ -17,23 +20,35 @@ class CalcBMIPage extends StatelessWidget {
             ),
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text("Calculate your BMI"),
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                  BMIInputField(
-                    controller: TextEditingController(),
-                    inputName: "weightInput",
-                  ),
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                  BMIInputField(
-                    controller: TextEditingController(),
-                    inputName: "heightInput",
-                  ),
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                  FilledButton(onPressed: () {}, child: Text("Calculate")),
-                ],
+              child: Form(
+                key: bmiProvider.formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Calculate your BMI"),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                    BMIInputField(
+                      controller: bmiProvider.weightController,
+                      inputName: "weightInput",
+                      bmiProvider: bmiProvider,
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                    BMIInputField(
+                      controller: bmiProvider.heightController,
+                      inputName: "heightInput",
+                      bmiProvider: bmiProvider,
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                    FilledButton(
+                      onPressed: () {
+                        if (bmiProvider.formKey.currentState!.validate()) {
+                          bmiProvider.calcBMI();
+                        }
+                      },
+                      child: Text("Calculate"),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
