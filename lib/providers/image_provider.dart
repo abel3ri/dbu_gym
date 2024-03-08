a48998cd122e399d63320de114394a6f2155accf
abel3ri <abelmerete@yahoo.com>
2024-03-08 23:56:45 +0300

    Add: image name argument

--- lib/providers/image_provider.dart
+++ lib/providers/image_provider.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class ProfileImageProvider with ChangeNotifier {
   String? _imagePath;
-
-  void setImagePath(String? path) {
+  String? _imageName;
+  void setImagePathAndName(String? path, String? name) {
     _imagePath = path;
+    _imageName = name;
     notifyListeners();
   }
 
   String? get imagePath => _imagePath;
+  String? get imageName => _imageName;
 }
