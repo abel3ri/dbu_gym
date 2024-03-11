12ed3972d594bf02b2871271adf9e22c5aa191b0
abel3ri <abelmerete@yahoo.com>
2024-03-11 22:38:35 +0300

    Add: error class for displaying errors

---
+++ lib/utils/error.dart
@@ -0,0 +1,25 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:flutter/material.dart';
+
+class CustomError {
+  String errorTitle;
+  String errorBody;
+  CustomError({
+    required this.errorTitle,
+    required this.errorBody,
+  });
+  void showError(BuildContext context) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        behavior: SnackBarBehavior.floating,
+        content: AwesomeSnackbarContent(
+            title: errorTitle,
+            message: errorBody,
+            contentType: ContentType.failure),
+      ),
+    );
+  }
+}
